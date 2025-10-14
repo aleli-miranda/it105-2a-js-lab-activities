16b2a0a1893a1c6f157b1a4abc19dc826050f519
alelimiranda227 <alelimiranda227@gmail.com>
2025-10-15 07:00:58 +0800

    Week 11 Lab Activities

---
+++ week11-activity-3.js
@@ -0,0 +1,45 @@
+//ARRAY
+/*We are setting up our small library of books 
+about JavaScript programming. 
+We have three books and want to prepare a list of them. 
+We will store three pieces information about each book: 
+title, author, and number of pages:
+
+Speaking JavaScript, Axel Rauschmayer, 460;
+Programming JavaScript Applications, Eric Elliott, 254;
+Understanding ECMAScript 6, Nicholas C. Zakas, 352.
+
+Create an array of three objects representing the books. 
+Each object must have the following properties: title, author, pages.*/
+
+
+
+
+
+/* Add a new book to the collection: 
+Learning JavaScript Design Patterns, by Addy Osmani, 254 pages. 
+Use the appropriate method to do this, 
+which will attach the book at the end of the array. 
+Display the length of the array and, in turn, all the book names in the collection.*/
+
+
+
+
+
+
+//Use the slice command to copy the last two books to the new array.
+
+
+
+
+/*The first book from the collection is lost in unexplained circumstances. 
+You have already accepted the loss, so remove it from the array. 
+Which method will you use for this purpose? 
+Display the length of the array and all the names of the books from the collection in turn.*/
+
+
+
+
+
+//Display the sum of the pages of all the books from the collection.
+
