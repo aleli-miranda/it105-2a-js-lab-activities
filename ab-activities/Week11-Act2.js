f19892265382928a3fd68f33cde0a7516dc7cb64
abigaylerimando-del <abigayle.rimando@olivarezcollegetagaytay.edu.ph>
2025-11-04 18:28:05 +0800

    Answered Activity

---
+++ ab-activities/Week11-Act2.js
@@ -0,0 +1,19 @@
+// Question 1: Create a train ticket object
+let ticket = {
+  from: "Orchard Road",   
+  to: "Sentosa",     
+  price: 80.00             
+};
+
+console.log(ticket.from);
+console.log(ticket.to);
+console.log(ticket.price);
+
+// Question 2: Declare empty object and add fields
+let person = {};
+
+person.name = "Abby";          
+person.surname = "Rimando";       
+
+console.log(person.name);
+console.log(person.surname);
