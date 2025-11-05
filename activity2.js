1f99efaf8c1b119a26e5a6ff4abf44df44789d13
johndavebernardino-coder <johndave.bernardino@olivarezcollegetagaytay.edu.ph>
2025-11-05 16:01:44 +0800

    Answered Activity

---
+++ activity2.js
@@ -0,0 +1,22 @@
+// Question 1
+let ticket = {
+  from: "Alfonso",
+  to: "Nasugbu Batangas",
+  price: 60
+};
+console.log(ticket.from);
+console.log(ticket.to);
+console.log(ticket.price);
+
+/*Question 2: 
+Declare an empty object and save it to a person variable. 
+Using dot notation, add the name and surname fields to the 
+object by entering your data as values. 
+Try to display the individual fields on the console. */
+
+// Question 2
+let person = {};
+person.name = "John Dave";
+person.surname = "Bernardino";
+console.log(person.name);
+console.log(person.surname);
