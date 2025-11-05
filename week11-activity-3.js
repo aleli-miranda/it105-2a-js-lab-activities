4839d128eead77b27cd81edce435e8a6d2639e4c
roseannbueno-hue <roseann.bueno@olivarezcollegetagaytay.edu.ph>
2025-11-05 16:49:08 +0800

    Add lab activity files for Week 11 - bueno-roseann

--- week11-activity-3.js
+++ week11-activity-3.js
@@ -1,57 +1,50 @@
 //ARRAY
-/*We are setting up our small library of books 
-about JavaScript programming. 
-We have three books and want to prepare a list of them. 
-We will store three pieces information about each book: 
-title, author, and number of pages:
-
-Speaking JavaScript, Axel Rauschmayer, 460;
-Programming JavaScript Applications, Eric Elliott, 254;
-Understanding ECMAScript 6, Nicholas C. Zakas, 352.
-
-Create an array of three objects representing the books. 
-Each object must have the following properties: title, author, pages.*/
-
 // Create an array of three objects representing the books
-let books = [
-  { title: "Speaking JavaScript", author: "Axel Rauschmayer", pages: 460 },
-  { title: "Programming JavaScript Applications", author: "Eric Elliott", pages: 254 },
-  { title: "Understanding ECMAScript 6", author: "Nicholas C. Zakas", pages: 352 }
+let books = [{
+        title: "Speaking JavaScript",
+        author: "Axel Rauschmayer",
+        pages: 460
+    },
+    {
+        title: "Programming JavaScript Applications",
+        author: "Eric Elliott",
+        pages: 254
+    },
+    {
+        title: "Understanding ECMAScript 6",
+        author: "Nicholas C. Zakas",
+        pages: 352
+    }
 ];
 
-/* Add a new book to the collection: 
-Learning JavaScript Design Patterns, by Addy Osmani, 254 pages. 
-Use the appropriate method to do this, 
-which will attach the book at the end of the array. 
-Display the length of the array and, in turn, all the book names in the collection.*/
+console.log("Book Collection:");
+console.log(books);
 
-// Add a new book to the collection using push()
-books.push({ title: "Learning JavaScript Design Patterns", author: "Addy Osmani", pages: 254 });
+// Add a new book to the collection
+books.push({
+    title: "Learning JavaScript Design Patterns",
+    author: "Addy Osmani",
+    pages: 254
+});
 
-// Display the length of the array and all the book names
+console.log("\nAfter adding a new book:");
 console.log("Length of the array:", books.length);
+console.log("Book names:");
 books.forEach(book => console.log(book.title));
 
-//Use the slice command to copy the last two books to the new array.
-
-// Use slice() to copy the last two books to a new array
+// Use the slice command to copy the last two books to the new array
 let lastTwoBooks = books.slice(-2);
-console.log("Last two books:", lastTwoBooks);
+console.log("\nLast two books copied to a new array:");
+console.log(lastTwoBooks);
 
-/*The first book from the collection is lost in unexplained circumstances. 
-You have already accepted the loss, so remove it from the array. 
-Which method will you use for this purpose? 
-Display the length of the array and all the names of the books from the collection in turn.*/
-
-// Remove the first book from the collection using shift()
+// Remove the first book from the array
 books.shift();
-// Display the length of the array and all the book names after removal
-console.log("Length after removal:", books.length);
-books.forEach(book => console.log(book.title));
 
-//Display the sum of the pages of all the books from the collection.
+console.log("\nAfter removing the first book:");
+console.log("Length of the array:", books.length);
+console.log("Book names:");
+books.forEach(book => console.log(book.title));
 
 // Display the sum of the pages of all the books from the collection
 let totalPages = books.reduce((sum, book) => sum + book.pages, 0);
-console.log("Total pages:", totalPages);
-
+console.log("\nSum of the pages of all books:", totalPages);
