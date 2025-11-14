64c886202900bf7f7e2499be83362d1f91cfa630
princesdonita <princesdonita.villanueva@olivarezcollegetagaytay.edu.ph>
2025-11-14 16:07:40 +0800

    Update week11-activity-2.js

--- week11-activity-2.js
+++ week11-activity-2.js
@@ -13,15 +13,6 @@ Then display the values of all fields of the ticket on the console.
 */
 //start code here
 
-// Question 1
-let ticket = {
-  from: "Penn Station",
-  to: "Philadelphia 30th Street",
-  price: 50
-};
-console.log(ticket.from);
-console.log(ticket.to);
-console.log(ticket.price);
 
 /*Question 2: 
 Declare an empty object and save it to a person variable. 
@@ -29,10 +20,4 @@ Using dot notation, add the name and surname fields to the
 object by entering your data as values. 
 Try to display the individual fields on the console. */
 
-// Question 2
-let person = {};
-person.name = "Princes";
-person.surname = "Villanueva";
-console.log(person.name);
-console.log(person.surname);
 
